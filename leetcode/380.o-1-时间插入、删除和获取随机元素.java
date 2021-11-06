713d96248e149739e7d538352346531a41571daf
2507697169@qq.com <2507697169@qq.com>
2021-11-06 13:23:20 +0800

    main

---
+++ leetcode/380.o-1-时间插入、删除和获取随机元素.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=380 lang=java
+ *
+ * [380] O(1) 时间插入、删除和获取随机元素
+ */
+
+// @lc code=start
+class RandomizedSet {
+
+    public RandomizedSet() {
+
+    }
+    
+    public boolean insert(int val) {
+
+    }
+    
+    public boolean remove(int val) {
+
+    }
+    
+    public int getRandom() {
+
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+// @lc code=end
+
